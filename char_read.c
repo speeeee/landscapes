0bfee4536a672877de09d4aa1952cebe8e695c23
Spencer <snsallay@gmail.com>
2016-07-24 13:11:01 -0400

    program now works as expected given current main function

--- char_read.c
+++ char_read.c
@@ -22,13 +22,16 @@ img read_img(FILE *);
 int ceq(col a, col b) { return (a.r==b.r&&a.g==b.g&&a.b==b.b); }
 
 // load BMP
-img read_img(FILE *f) { int8_t header[54];
-  if(fread(header,54,1,f)!=54) { printf("error: could not read file\n"); exit(0); }
-  img im; int pos = *(int*)&(header[0x0A]);
-  im.w = *(int32_t *)&(header[0x12]); im.h = *(int32_t *)&(header[0x16]);
-  int8_t *dat; fread(dat,im.w*im.h*3,1,f);
-  for(int i=0;i<im.w*im.h*3;i+=3) { 
-    im.dat[i/3] = (col) { dat[i], dat[i+1], dat[i+2] }; }
+img read_img(FILE *f) { uint8_t header[54];
+  if(fread(header,1,54,f)!=54) { printf("error: could not read file\n"); /*exit(0);*/ }
+  img im; int pos = *(int*)&(header[0x0A]); int bpp = *(int*)&(header[0x1C])/8;
+  im.w = *(int32_t *)&(header[0x12]); im.h = abs(*(int32_t *)&(header[0x16]));
+  int isz = im.w*im.h*bpp;
+  uint8_t *dat = malloc(isz*sizeof(uint8_t));
+  int e = fread(dat,1,isz,f);
+  im.dat = malloc(isz/bpp*sizeof(col));
+  for(int i=0;i<isz;i+=bpp) { 
+    im.dat[i/bpp] = (col) { dat[i], dat[i+1], dat[i+2] }; }
   return im; }
 
 //          bool val of point, width of pt, height of pt, index of pt, image
@@ -72,5 +75,6 @@ int main(int argc,char **argv) { G_ftbl = malloc((++G_fsz)*sizeof(gra));
   int *bl = malloc(9*sizeof(int)); for(int i=0;i<9;i++) { bl[i] = i%2; }
   G_ftbl[0] = (gra) { (tile) { bl, (col) { 0, 255, 0 }, 3, 3 },
                       (fun) { "0", (fra) { 0,0,0,0 }, (fra) { 0,0,0,0 }, 1, 1 } };
-  FILE *g = fopen("graph.bmp","r");
-  img bg = read_img(g); fclose(g); return 0; }
+  FILE *g = fopen("graph.bmp","rb");
+  img bg = read_img(g); //printf("%i\n",(int)bg.dat[2].g);
+  fclose(g); return 0; }
