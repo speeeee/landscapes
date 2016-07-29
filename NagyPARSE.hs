d816c2e1daccdbf28f66f648186ff146a2998d2d
Spencer <snsallay@gmail.com>
2016-07-29 00:12:16 -0400

    comment out non-working code that is in progress (add grouping function for parsing)

--- NagyPARSE.hs
+++ NagyPARSE.hs
@@ -7,6 +7,8 @@ data Img = Img Int Int [[[Int]]] deriving (Show,Eq)
 data Frame = Int Int Int Int deriving (Show,Eq)
 data Fun = Img [Char] Frame Bool deriving (Show,Eq) -- the Bool is to confirm dead-end
 
+funs = ["+",":"]
+
 main = do
   (_,Just plst,_,_) <-
     createProcess (proc "./char_read" []) { std_out = CreatePipe }
@@ -15,23 +17,32 @@ main = do
       e = imgCmpS g (extr (1,1,3,3) b) [0,249,0,255]
   putStrLn $ show e
 
--- function will be cleaned.
+-- WIP
+{--- function will be cleaned.
 eval :: Img -> Frame -> [Fun] -> [Char]
-eval i f fs = eval' i f fs i
-eval' :: Img -> Frame -> [Fun] -> Img -> [Char]
-eval' = execF . uncurry evalArg . toF . flip extrf
+eval i f fs = eval' i f fs i f
+eval' :: Img -> Frame -> [Fun] -> Img -> Frame -> [Char]
+eval' = uncurry execF . uncurry toF . flip extrf
 
-toF :: Img -> [Fun] -> (Fun,[Fun])
-toF i fs = case find (flip imgCmpS i) fs of
-  Just a -> (a,fs)
+toF :: Frame -> Img -> [Fun] -> (Fun,[Fun])
+toF fr (Img w h d) fs = case find (flip imgCmpS (Img w h d)) fs of
+  Just (Fun (Img wi hi _) p f g) -> (Fun (Img w h d) p (fsc (w/wi) (h/hi) fr) g,fs)
   Nothing -> (Fun (Img 0 0 []) "ERROR_NO_FUNCTION" (Frame 0 0 0 0) False,fs)
 
-evalArg :: Fun -> [Fun] -> Img -> [Char]
-evalArg (Fun _ _ _ False) _ _ = f
-evalArg (Fun _ f arg _) fs i = concat [f," ",eval i arg fs]
+evalArg :: Fun -> [Fun] -> Img -> Frame -> [Char]
+evalArg (Fun _ _ _ _ False) _ _ _ = f
+evalArg (Fun _ f arg (sx,sy) _) fs i fr = concat [f," ",eval i arg fs]
 
-execF :: [Char] -> [Char]
-execF = id
+-- + : I@ -1 0 1 1 list I@ 1 0 1 1
+execF :: Fun -> [Fun] -> [Char]
+execF (Fun _ p f _) fs = groupF $ words p
+
+-- + (: (I@ -1 0 1 1 (list I@ 1 0 1 1)))
+groupF :: [Char] -> [[Char]]
+groupF = foldr (\(k,(n:ns)) -> if k`elem`funs then []:n:ns else (k:n):ns)
+
+fsc :: Int -> Int -> Frame -> Frame
+fsc w h (Frame xi yi wi hi) = (Frame xi yi (wi*w) (hi*h))-}
 
 toImgs :: [[Char]] -> [Img]
 toImgs = flip toImgs' []
@@ -62,8 +73,8 @@ extr' (x,y,w,h) = map (\k -> take w $ drop x k) . take h . drop y
 extr :: (Int, Int, Int, Int) -> Img -> Img
 extr (x,y,w,h) (Img bw bh dat) = Img w h (extr' (x,y,w,h) dat)
 
-extrf :: Frame -> Img -> Img
-extrf (Frame x y w h) = extr (x,y,w,h)
+extrf :: Frame -> Img -> (Frame,Img)
+extrf (Frame x y w h) = ((,) (Frame x y w h)) . extr (x,y,w,h)
 
 mtob :: Maybe Bool -> Bool
 mtob (Just a) = a
