f0ec34194bbc3f3aaa8e7617ccfa917cc31803a5
Spencer <snsallay@gmail.com>
2016-08-11 00:15:41 -0400

    add standard functions

--- hangzik.hs
+++ hangzik.hs
@@ -4,24 +4,34 @@ import Data.List.Split
 
 import Debug.Trace
 
-data Fun = Fun [[Char]] ([[Char]] -> [[Char]])
+data Fun = Fun { na :: [Char], fn :: [[Char]], p :: ([[Char]] -> [[Char]]) }
 
-prims :: [Fun]
-prims = []
+prims :: [Fun] -> [Fun]
+prims fs = 
+  [Fun "op" [] (\(o:a:b:n) -> case find ((o==) . fst) 
+                                     [("+'",(+)),("-'",(-)),("*'",(*)),("/'",div)] of
+                              Just (_,f) -> str $ f (read a::Int) (read b::Int):n
+                              Nothing -> n)
+  ,Fun "!" [] (\(q:n) -> parse (reverse $ lex $ replace '|' '"' q) $ zip [fs] n)
+  ,Fun "?" [] (\(qt:qf:c:n) -> (if c=="True" then qt else qf):n)
+  ,Fun "sw" [] (\(a:b:n) -> b:a:n), Fun "dr" [] tail
+  ,Fun "du" [] (\(a:n) -> a:a:n), Fun "pick" [] (\(a:b:c:n) -> c:a:b:c:n)]
 
 main = do
   (i:o) <- getArgs
-  flip parse prims <$> (reverse <$> (lex <$> readFile i)) >>= writeFile o
+  flip parse (prims []) <$> (reverse <$> (lex <$> readFile i)) >>= writeFile o
 
 lex :: [Char] -> [[Char]]
 lex = chop (\xl@(x:_) -> case x of ' '  -> span (' '==) xl
                                    '"'  -> span ('"'==) xl
                                    _    -> span (' '/=) xl)
 
-parse :: [[Char]] -> [([Fun],[Char])] -> [[Char]]
+parse :: [[Char]] -> [([Fun],[[Char]])] -> [[Char]]
 --parse = foldr (\k (f,n) -> appf n <$> find (k==) f >>= (f,k:n)) [[],[]]
-parse = flip foldr(\k (f,n) -> case find (k==) f of (Just a) -> appf f a n
-                                                     _ -> (f,k:n))
+parse = flip foldr (\k (f,n) -> case find (k==) (map na (f++prims f)) of 
+                                (Just a) -> appf f a n
+                                _ -> (f,k:n))
 
-appf :: [Fun] -> Fun -> [[Char]]
-appf fs (Fun o s) n = if empty o then s n else parse o $ zip [fs] n
+appf :: [Fun] -> Fun -> ([Fun],[[Char]])
+appf fs (Fun "DEF" _ _) (name:q:n) = ((Fun name q id:fs),n)
+appf fs (Fun _ o s) n = if empty o then (fs,s n) else (fs,parse o $ zip [fs] n)
